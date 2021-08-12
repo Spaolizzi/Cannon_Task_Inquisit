db2ad401c0a5d569ae94d84b1d51802a1e6b6589
Sophie Paolizzi <sophie_paolizzi@Sophies-MBP.lan>
2021-08-12 14:09:49 -0400

    laptop changes

--- DrawArcs/arcdraw.m
+++ DrawArcs/arcdraw.m
@@ -10,44 +10,39 @@ close all
 figsize=500;
 min_arc=1; %Smallest arc angle
 max_arc=60; %Largest arc angle
-d_arc=10; %step size
-ARC=min_arc:d_arc:max_arc;
-for ct1=1:length(ARC)
-%% Options
-%Define the green arc
-theta_max=ARC(ct1); %Angle of the arc
-thickness=20; %thickness of green arc
-theta_arc=linspace(0,theta_max,200); %Angles of arc
-
-%Define the gray circle
-theta_circ=linspace(0,2*pi,200);%angles in circle
-thickness_circ=0;% thickness of the circle
-circ_coords=[cos(theta_circ);sin(theta_circ)];
-circ_color=0.4; %"Grayness." of the circle (0 is black, 1 is white)
-ROT = pi/2 + ct1*pi/180;
-%Angular spacing of arc over images
-dtheta=5*pi/180; %degrees converted to radians%Vector of angles to rotate arc through
-filedir=strcat(cd,'/Arcs/');%Directory string
-for ct=1:length(ROT)
-    theta=theta_arc*pi/180 - ROT(ct);
+d_arc=1; %step size
+all_arcs=min_arc:d_arc:max_arc;
+for ct=1:length(all_arcs)
+    arc=all_arcs(ct);
+    %% Options
+    %Define the sheild
+    thickness=17; %thickness of green arc
+    arc_angles=linspace(-arc/2,arc/2,200); %Angles of arc
+    
+    %Define the gray circle
+    theta_circ=linspace(0,2*pi,200);%angles in circle
+    thickness_circ=15;% thickness of the circle
+    circ_coords=[cos(theta_circ);sin(theta_circ)];
+    circ_color=0.4; %"Grayness" of the circle (0 is black, 1 is white)
+    
+    %Angular spacing of arc over images
+    dtheta=10*pi/180; %degrees converted to radians
+    
+    filedir=strcat(cd,'\Arcs\');%Directory string
+    theta=arc_angles*pi/180+pi/2;
     x=[cos(theta);
-        sin(theta)];   
+        sin(theta)];
     fig=figure;
-    hold on   
-    plot(x(1,:),x(2,:),'g','LineWidth', thickness)
-    axis equal
-    axis([-1.2,1.2,-1.2,1.2])   
+    hold on
+    plot(x(1,:),x(2,:),'g','LineWidth',thickness)
+    axis square
+    axis([-1.2,1.2,-1.2,1.2])
     %Turn figure and axes backgrounds transparent
     ax=gca;
-    fig.Units='pixels';
-    ax.Units='pixels';
-    fig.Position(3:4)=[figsize,figsize];
-    ax.Position=[0,0,figsize,figsize];
-    ax.Visible='off';
+    fig.Position(3)=fig.Position(4);
     ax.Color='none';
     fig.Color='none';
-%         saveas(fig,strcat(filedir,sprintf('Arc%i',thetaMax(ct))),'epsc')%EPS Format
-saveas(fig,strcat(filedir,sprintf('Arc%i',ARC(ct1))),'png')%SVG Format
-close all
-end
+    ax.Visible='off';
+    saveas(fig,strcat(filedir,sprintf('Arc%i',arc)),'png')%SVG Format
+    close all
 end
---
+++ DrawArcs/arcdraw_orig.m
@@ -0,0 +1,55 @@
+%%ArcDraw.m
+%
+% This function generates a series of vectorized graphics images of an arc
+% overlaid onto a gray circle
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+clc
+clear all
+close all
+figsize=500;
+min_arc=0; %Smallest arc angle
+max_arc=1; %Largest arc angle
+d_arc=1; %step size
+ARC=min_arc:d_arc:max_arc;
+for ct1=1:length(ARC)
+%% Options
+%Define the green arc
+theta_max=ARC(ct1); %Angle of the arc
+thickness=20; %thickness of green arc
+%theta_arc=linspace(0,theta_max,200); %Angles of arc
+arc_angles=linspace(-arc/2,arc/2,200);
+
+%Define the gray circle
+theta_circ=linspace(0,2*pi,200);%angles in circle
+thickness_circ=0;% thickness of the circle
+circ_coords=[cos(theta_circ);sin(theta_circ)];
+circ_color=0.4; %"Grayness" of the circle (0 is black, 1 is white)
+
+%Angular spacing of arc over images
+dtheta=5*pi/180; %degrees converted to radians
+ROT= pi/2-ct1*pi/180;%Vector of angles to rotate arc through
+filedir=strcat(cd,'/Arcs/');%Directory string
+for ct=1:length(ROT)
+    theta=arc_angles*pi/180+pi/2;
+    x=[cos(theta);
+        sin(theta)];   
+    fig=figure;
+    hold on   
+    plot(x(1,:),x(2,:),'g','LineWidth', thickness)
+    axis equal
+    axis([-1.2,1.2,-1.2,1.2])   
+    %Turn figure and axes backgrounds transparent
+    ax=gca;
+    fig.Units='pixels';
+    ax.Units='pixels';
+    fig.Position(3:4)=[figsize,figsize];
+    ax.Position=[0,0,figsize,figsize];
+    ax.Visible='off';
+    ax.Color='none';
+    fig.Color='none';
+%         saveas(fig,strcat(filedir,sprintf('Arc%i',thetaMax(ct))),'epsc')%EPS Format
+saveas(fig,strcat(filedir,sprintf('Arc%i',ARC(ct1))),'png')%SVG Format
+close all
+end
+end
